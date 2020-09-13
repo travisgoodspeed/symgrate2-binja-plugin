c6b6818d56ac59b6ee9c59e7629e3010402e4fb4
Travis Goodspeed <travis@radiantmachines.com>
2020-09-13 10:39:27 -0400

    Queries now run in a background thread, without interrupting the GUI.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python3
+
 from binaryninja import *
+
+import sys;
 
 from .Symgrate2 import Symgrate2
 
 LEN = 18;
 
 def functionprefix(bv,fun):
-    """Returns the first eighteen bytes of a function as ASCII."""
-    B=bv.read(fun.start, LEN);
-    raw="";
-    for i in range(0, LEN, 2):
-        h=int(B[i+1]);
-        l=int(B[i]);
-        raw+="%02x%02x"%(l,h);
-    #print("raw:     %s"%raw);
-    return raw;
+        """Returns the first eighteen bytes of a function as ASCII."""
+        B=bv.read(fun.start, LEN);
+        raw="";
+        for i in range(0, LEN, 2):
+                h=int(B[i+1]);
+                l=int(B[i]);
+                raw+="%02x%02x"%(l,h);
+        #print("raw:     %s"%raw);
+        return raw;
 
 
 def function_search(bv,f):
@@ -29,20 +33,37 @@ def function_search(bv,f):
                 show_message_box("Symgrate2 Query", "Unknown function.",
                                  MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
 
-def allfunction_search(bv):
-        """Searches for all functions."""
-        count=0;
 
-        for f in bv.functions:
-                pre=functionprefix(bv,f);
-                name=Symgrate2.queryfn(pre);
+def allfunction_searchbg(bv):
+        """Searches for all functions in the background."""
+        # Start a solver thread for the path associated with the view
+        s = Solver(bv)
+        s.start()
 
-                if name!=None:
-                        print("found %s"%name);
-                else:
-                        print("Missing %s"%pre);
 
+class Solver(BackgroundTaskThread):
+        def __init__(self, bv):
+                BackgroundTaskThread.__init__(self, "Searching Symgrate2.", True);
+                self.bv=bv;
+        def run(self):
+                """Searches for all functions."""
+                count=0;
+                matches=0;
+                bv=self.bv;
+                
+                for f in bv.functions:
+                        pre=functionprefix(bv,f);
+                        name=Symgrate2.queryfn(pre);
+
+                        count=count+1;
+                        if name!=None:
+                                #print("found %s"%name);
+                                print("%08x %-20s" % (f.start, name));
+                                matches=matches+1;
+                        self.progress=("Searched %d functions on Symgrate2, %d matches." %(count,matches))
+
+        
 PluginCommand.register_for_function("Symgrate2 Function Search", "Searches Symgrate2 db for the current function.", function_search)
-PluginCommand.register("Symgrate2 Program Search", "Searches Symgrate2 db for all functions.", allfunction_search)
+PluginCommand.register("Symgrate2 Program Search", "Searches Symgrate2 db for all functions.", allfunction_searchbg)
 
 
